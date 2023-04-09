6af8a9438a80633e4baa82f50be1df90a0c9a384
tkex <tikado@web.de>
2023-04-09 10:51:57 +0200

    feat: add few comments and inspector tooltips

--- Assets/Assets/Code/EntranceController.cs
+++ Assets/Assets/Code/EntranceController.cs
@@ -5,20 +5,25 @@ using DG.Tweening;
 
 public class EntranceController : MonoBehaviour
 {
-    public GameObject entranceGo;
-   
-    public float delay = 1f;
-    public float duration = 3f;
-    public float distance = 3f;
+    public GameObject entranceGo; // The game object representing the door
 
-    private Vector3 originalPosition;
-    private Vector3 targetPosition;
+    [Header("Einfahrt-Einstellungen")]
+    [Tooltip("Delay before the start of the vertical door movement")]
+    public float delay = 1f; // Delay before the door movement starts
+    [Tooltip("Duration of the vertical door movement")]
+    public float duration = 3f; // Duration of the door movement
+    [Tooltip("Distance of the vertical door movement")]
+    public float distance = 3f; // Distance by which the door should move
+
+    private Vector3 originalPosition; // Original position of the door
+    private Vector3 targetPosition; // Target position of the door
 
     private void Start()
     {
-        originalPosition = entranceGo.transform.position;
-        targetPosition = originalPosition + Vector3.up * distance;
+        originalPosition = entranceGo.transform.position; // store the original position of the door
+        targetPosition = originalPosition + Vector3.up * distance; // calculate the target position of the door
 
+        // Start the door movement
         entranceGo.transform.DOMove(targetPosition, duration)
             .SetDelay(delay)
             .SetEase(Ease.OutQuad);
@@ -26,12 +31,14 @@ public class EntranceController : MonoBehaviour
 
     public void OpenDoor()
     {
+        // Door movement to open the door
         entranceGo.transform.DOMove(targetPosition, duration)
             .SetEase(Ease.OutQuad);
     }
 
     public void CloseDoor()
     {
+        // Door movement to close the door
         entranceGo.transform.DOMove(originalPosition, duration)
             .SetEase(Ease.OutQuad);
     }

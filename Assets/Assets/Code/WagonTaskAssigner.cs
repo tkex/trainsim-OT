20ad00bb84a31d08ae704906f81c8cb21616576e
tkex <tikado@web.de>
2023-04-14 13:23:20 +0200

    feat: add working wagon state machine based on tasks system

--- Assets/Assets/Code/WagonTaskAssigner.cs
+++ Assets/Assets/Code/WagonTaskAssigner.cs
@@ -21,9 +21,7 @@ public class WagonTaskAssigner : MonoBehaviour
 {
 
     [Header("WagonTaskAssigner Settings")]
-
-    [Tooltip("The maintenance state of the wagon.")]
-    public WagonStates wagonState;
+    
     [Tooltip("List of tasks and their status.")]
     public List<WagonTask> tasks = new List<WagonTask>();
     [Tooltip("The max. possible task a wagon can have.")]
@@ -33,49 +31,9 @@ public class WagonTaskAssigner : MonoBehaviour
 
     private void Start()
     {
-        // Set default maintenance state of wagon as not maintained.
-        wagonState =  WagonStates.NotMaintained;
-
         // Assign random tasks to the wagon.
         AssignTasksToWagon();
     }
-
-    private void Update()
-    {
-        // Check if all tasks are completed and update the wagon state accordingly.
-        bool allTasksCompleted = true;
-        foreach (WagonTask task in tasks)
-        {
-            if (!task.isDone)
-            {
-                allTasksCompleted = false;
-                break;
-            }
-        }
-        if (allTasksCompleted)
-        {
-            switch (wagonState)
-            {
-                case WagonStates.NotMaintained:
-                    wagonState = WagonStates.Maintained;
-                    break;
-                case WagonStates.InProgress:
-                    wagonState = WagonStates.Maintained;
-                    break;
-            }
-        }
-        else
-        {
-            switch (wagonState)
-            {
-       
-                case WagonStates.NotMaintained:
-                    wagonState = WagonStates.InProgress;
-                    break;
-            }
-        }
-    }
-
 
     // This method assigns between 1 and 5 tasks to the wagon and sets their isDone field to false.
     public void AssignTasksToWagon()
@@ -116,35 +74,6 @@ public class WagonTaskAssigner : MonoBehaviour
         }
 
         // Mark tasks as initialized to avoid assigning them again.
-        isTasksInitialized = true;
-
-        // Subscribe to the TaskCompleted event for each task.
-        foreach (WagonTask task in tasks)
-        {
-            task.TaskCompleted += OnTaskCompleted;
-        }
-    }
-
-    private void OnTaskCompleted(WagonTask completedTask)
-    {
-        // Check if all tasks are completed.
-        bool allTasksCompleted = true;
-        foreach (WagonTask task in tasks)
-        {
-            if (!task.isDone)
-            {
-                allTasksCompleted = false;
-                break;
-            }
-        }
-
-        if (allTasksCompleted)
-        {
-            wagonState = WagonStates.Maintained;
-        }
-        else
-        {
-            wagonState = WagonStates.InProgress;
-        }
+        isTasksInitialized = true;    
     }
 }
---
+++ Assets/Assets/Code/WagonTaskStateMachine.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+
+// This script manages the maintenance state of a wagon based on its tasks completion status
+public class WagonTaskStateMachine : MonoBehaviour
+{
+    // The wagon task assigner component attached to the wagon
+    private WagonTaskAssigner wagonTaskAssigner;
+
+    // The maintenance state of the wagon
+    [Tooltip("The maintenance state of the wagon.")]
+    public WagonStates wagonState;
+
+    private void Start()
+    {
+        // Get the WagonTaskAssigner component from the same GameObject
+        wagonTaskAssigner = GetComponent<WagonTaskAssigner>();
+
+        // Set default maintenance state of wagon as not maintained.
+        wagonState = WagonStates.NotMaintained;
+    }
+
+    private void Update()
+    {
+        // Check if the state needs to be updated
+        if (wagonTaskAssigner.tasks.Count == 0 || !CheckIfAnyTasksCompleted())
+        {
+            // If no tasks or no task is completed, set wagon state as not maintained
+            ChangeState(WagonStates.NotMaintained);
+        }
+        else if (CheckIfAllTasksCompleted())
+        {
+            // If all tasks are completed, set wagon state as maintained
+            ChangeState(WagonStates.Maintained);
+        }
+        else if (wagonState != WagonStates.InProgress)
+        {
+            // If some tasks are completed, set wagon state as in progress
+            ChangeState(WagonStates.InProgress);
+        }
+    }
+
+    private void ChangeState(WagonStates newState)
+    {
+        // Set wagon state
+        wagonState = newState;
+
+        // Do something with the new state, for example update the wagon graphics
+        Debug.Log(gameObject.name + " state changed to: " + newState);
+    }
+
+    private bool CheckIfAllTasksCompleted()
+    {
+        // Check if all tasks are completed
+        foreach (WagonTask task in wagonTaskAssigner.tasks)
+        {
+            if (!task.isDone)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool CheckIfAnyTasksCompleted()
+    {
+        // Check if any task is completed
+        foreach (WagonTask task in wagonTaskAssigner.tasks)
+        {
+            if (task.isDone)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}

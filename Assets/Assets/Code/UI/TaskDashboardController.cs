07e87599ba56c782fdc78af77eec9c82250388ae
tkex <tikado@web.de>
2023-04-19 07:39:24 +0200

    feat: add proper formatting and colouring for dashboard

--- Assets/Assets/Code/UI/TaskDashboardController.cs
+++ Assets/Assets/Code/UI/TaskDashboardController.cs
@@ -1,10 +1,11 @@
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
 public class TaskDashboardController : MonoBehaviour
 {
     public TrainController trainController;
     public TextMeshProUGUI dashboardText;
+
 
     private void Update()
     {
@@ -15,14 +16,15 @@ public class TaskDashboardController : MonoBehaviour
         foreach (GameObject wagon in trainController.wagons)
         {
             // Display the wagon parent object name in bold color
-            dashboardText.text += $"<b><color=#FFD700>{wagon.gameObject.name}</color></b>\n";
+            dashboardText.text += $"<b><color=#FFD700>{wagon.gameObject.name}:</color></b>\n";
 
             // Loop through all tasks in the WagonTaskAssigner component
             WagonTaskAssigner taskAssigner = wagon.GetComponent<WagonTaskAssigner>();
             foreach (WagonTask task in taskAssigner.tasks)
             {
                 // Display the task type with checkbox
-                string checkbox = task.isDone ? "[X]" : "[ ]";
+                string checkbox = task.isDone ? "<color=#00FF00>[x]</color>" : "[ ]";
+
                 string text = $"{checkbox} {task.taskType}";
 
                 // Cross out the task text if it's done

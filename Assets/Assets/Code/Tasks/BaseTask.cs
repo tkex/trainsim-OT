269a952cebab5a3514985b49e9ec5eab29415e9b
tkex <tikado@web.de>
2023-04-28 05:38:29 +0200

    refactor: remove basetask since its not needed anymore

--- Assets/Assets/Code/Tasks/BaseTask.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BaseTask
-{
-    public abstract void Spawn();
-    public abstract void Complete();
-}

707fd812036c52c6460a3da4d9c02315e11b206c
tkex <tikado@web.de>
2023-05-27 06:00:43 +0200

    feat: add horizontal sliding for double doors

--- Assets/Assets/Code/EntranceController.cs
+++ Assets/Assets/Code/EntranceController.cs
@@ -5,21 +5,28 @@ using DG.Tweening;
 
 public class EntranceController : MonoBehaviour
 {
+
     [Header("Door Settings")]
-    [SerializeField] private GameObject movingDoor;
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject rightDoor;
     [SerializeField] private DoorType doorType;
     [SerializeField] private float delayForDoorClose = 1f;
     [SerializeField] private float duration = 3f;
     [SerializeField] private float distance = 3f;
 
-    private Vector3 doorOriginalPosition;
-    private Vector3 doorTargetPosition;
+    private Vector3 leftDoorOriginalPosition;
+    private Vector3 rightDoorOriginalPosition;
+    private Vector3 leftDoorTargetPosition;
+    private Vector3 rightDoorTargetPosition;
     private bool openDoor = false;
 
     private void Start()
     {
-        doorOriginalPosition = movingDoor.transform.position;
-        doorTargetPosition = doorOriginalPosition + Vector3.up * distance;
+        leftDoorOriginalPosition = leftDoor.transform.position;
+        rightDoorOriginalPosition = rightDoor.transform.position;
+
+        leftDoorTargetPosition = leftDoorOriginalPosition + Vector3.left * distance;
+        rightDoorTargetPosition = rightDoorOriginalPosition + Vector3.right * distance;
     }
 
     private void Update()
@@ -66,7 +73,8 @@ public class EntranceController : MonoBehaviour
 
     public void OpenDoor()
     {
-        movingDoor.transform.DOMove(doorTargetPosition, duration).SetEase(Ease.OutQuad);
+        leftDoor.transform.DOMove(leftDoorTargetPosition, duration).SetEase(Ease.OutQuad);
+        rightDoor.transform.DOMove(rightDoorTargetPosition, duration).SetEase(Ease.OutQuad);
         openDoor = true;
 
         // Close the door after delay
@@ -75,7 +83,8 @@ public class EntranceController : MonoBehaviour
 
     public void CloseDoor()
     {
-        movingDoor.transform.DOMove(doorOriginalPosition, duration).SetEase(Ease.OutQuad);
+        leftDoor.transform.DOMove(leftDoorOriginalPosition, duration).SetEase(Ease.OutQuad);
+        rightDoor.transform.DOMove(rightDoorOriginalPosition, duration).SetEase(Ease.OutQuad);
         openDoor = false;
     }
 
@@ -86,3 +95,91 @@ public class EntranceController : MonoBehaviour
         CloseDoor();
     }
 }
+
+/*
+ *  Old code for one door that goes up vertically.
+ * 
+ * 
+[Header("Door Settings")]
+[SerializeField] private GameObject movingDoor;
+[SerializeField] private DoorType doorType;
+[SerializeField] private float delayForDoorClose = 1f;
+[SerializeField] private float duration = 3f;
+[SerializeField] private float distance = 3f;
+
+private Vector3 doorOriginalPosition;
+private Vector3 doorTargetPosition;
+private bool openDoor = false;
+
+private void Start()
+{
+    doorOriginalPosition = movingDoor.transform.position;
+    doorTargetPosition = doorOriginalPosition + Vector3.up * distance;
+}
+
+private void Update()
+{
+    if (openDoor)
+    {
+        OpenDoor();
+    }
+    else
+    {
+        CloseDoor();
+    }
+}
+
+private void OnEnable()
+{
+    switch (doorType)
+    {
+        case DoorType.Entrance:
+            TrainController.OnTrainArriving += OpenDoor;
+            TrainController.OnTrainDeparting += CloseDoor;
+            break;
+        case DoorType.Exit:
+            TrainController.OnTrainDeparting += OpenDoor;
+            TrainController.OnTrainArriving += CloseDoor;
+            break;
+    }
+}
+
+private void OnDisable()
+{
+    switch (doorType)
+    {
+        case DoorType.Entrance:
+            TrainController.OnTrainArriving -= OpenDoor;
+            TrainController.OnTrainDeparting -= CloseDoor;
+            break;
+        case DoorType.Exit:
+            TrainController.OnTrainDeparting -= OpenDoor;
+            TrainController.OnTrainArriving -= CloseDoor;
+            break;
+    }
+}
+
+public void OpenDoor()
+{
+    movingDoor.transform.DOMove(doorTargetPosition, duration).SetEase(Ease.OutQuad);
+    openDoor = true;
+
+    // Close the door after delay
+    StartCoroutine(CloseDoorWithDelay(delayForDoorClose));
+}
+
+public void CloseDoor()
+{
+    movingDoor.transform.DOMove(doorOriginalPosition, duration).SetEase(Ease.OutQuad);
+    openDoor = false;
+}
+
+private IEnumerator CloseDoorWithDelay(float delay)
+{
+    yield return new WaitForSeconds(delay);
+
+    CloseDoor();
+}
+*/
+
+

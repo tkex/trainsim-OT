e5a67b182572773759a6ea6dec86cf795ab71456
tkex <tikado@web.de>
2023-05-28 14:11:57 +0200

    feat: enhance mop by switching materials by isClean bool flag

--- Assets/Assets/Code/MopScript.cs
+++ Assets/Assets/Code/MopScript.cs
@@ -7,6 +7,29 @@ public class MopScript : MonoBehaviour
 {
     public bool isClean = true;
 
+    public Material cleanMaterial;
+    public Material dirtMaterial;
+
+    private Renderer mopRenderer;
+
+    private void Start()
+    {
+        mopRenderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        if (isClean)
+        {
+            mopRenderer.material = cleanMaterial;
+        }
+        else
+        {
+            mopRenderer.material = dirtMaterial;
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         // If in CleanWater, mop is clean again

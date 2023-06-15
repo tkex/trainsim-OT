84bec63c5e27c15c4e4227ca933bbc821f97b55d
tkex <tikado@web.de>
2023-06-15 06:59:26 +0200

    feat: add proper delays for compass directions

--- Assets/Assets/Code/XRHelpers/PlayerNavigation.cs
+++ Assets/Assets/Code/XRHelpers/PlayerNavigation.cs
@@ -1,22 +1,29 @@
+using System.Collections;
 using UltimateXR.Guides;
 using UnityEngine;
 
 public class PlayerNavigation : MonoBehaviour
 {
-    public GameObject zoneA; 
+    // The trigger gameobject that have the navigationtriggerzone script on them
+    public GameObject zoneA;
     public GameObject zoneB;
 
+    // The gameobject the compass directs to
     public Transform targetObjectA;
     public Transform targetObjectB;
 
+    // Delay parameters for Compass
+    public float navigateToADelay = 3f;
+    public float navigateToBDelayWhenButtonIsClicked = 10f;
+
+    // Control flags for handling logic
     private bool navigateToA = false;
     private bool navigateToB = false;
     private bool reachedBothZones = false;
 
     void Start()
     {
-        UxrCompass.Instance.SetTarget(targetObjectA.transform, UxrCompassDisplayMode.Use);
-        navigateToA = true;
+        StartCoroutine(ActivateCompassToAAfterDelay(navigateToADelay));
     }
 
     void Update()
@@ -40,8 +47,8 @@ public class PlayerNavigation : MonoBehaviour
         if (!reachedBothZones)
         {
             navigateToA = false;
-            UxrCompass.Instance.SetTarget(targetObjectB.transform, UxrCompassDisplayMode.Use);
-            // navigateToB = true;
+            UxrCompass.Instance.SetTarget(null);
+            zoneB.SetActive(true);
         }
     }
 
@@ -50,16 +57,30 @@ public class PlayerNavigation : MonoBehaviour
         navigateToB = false;
         UxrCompass.Instance.SetTarget(null);
         reachedBothZones = true;
+        zoneB.SetActive(false);
     }
 
-    // When button is clicked in TrainControllerUI, call this one
+    private IEnumerator ActivateCompassToAAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        navigateToA = true;
+        UxrCompass.Instance.SetTarget(targetObjectA.transform, UxrCompassDisplayMode.Use);
+    }
+
+    // When button is clicked in TrainControllerUI, call this function to start the Coroutine for new Compass direction
     public void ButtonIsClicked()
     {
         if (!reachedBothZones)
         {
-            navigateToA = false;
-            navigateToB = true;
-            UxrCompass.Instance.SetTarget(targetObjectB.transform, UxrCompassDisplayMode.Use);
+            StartCoroutine(ActivateCompassToBAfterDelay(navigateToBDelayWhenButtonIsClicked));
         }
+    }
+
+    private IEnumerator ActivateCompassToBAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        navigateToA = false;
+        navigateToB = true;
+        UxrCompass.Instance.SetTarget(targetObjectB.transform, UxrCompassDisplayMode.Use);
     }
 }
